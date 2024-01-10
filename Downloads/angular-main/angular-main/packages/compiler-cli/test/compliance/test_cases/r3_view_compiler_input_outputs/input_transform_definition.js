d8dbe82cb973af07dadbd3f6cd167002ee740cb2
abhipatwari17 <88781436+abhipatwari17@users.noreply.github.com>
2024-01-10 12:07:00 +0530

    Delete Downloads/angular-main/angular-main directory

--- Downloads/angular-main/angular-main/packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_input_outputs/input_transform_definition.js
+++
@@ -1,8 +0,0 @@
-MyDirective.ɵdir = /*@__PURE__*/ $r3$.ɵɵdefineDirective({
-  …
-  inputs: {
-    functionDeclarationInput: [$r3$.ɵɵInputFlags.HasTransform, "functionDeclarationInput", "functionDeclarationInput", toNumber],
-    inlineFunctionInput: [$r3$.ɵɵInputFlags.HasTransform, "inlineFunctionInput", "inlineFunctionInput", (value, _) => value ? 1 : 0]
-  },
-  features: [$r3$.ɵɵInputTransformsFeature]…
-});
